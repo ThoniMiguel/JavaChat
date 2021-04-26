3a07415fc6ca4255ff531cfe36916047884f7162
ThoniMiguel <wanok8000@outlook.com>
2021-04-26 17:52:13 -0300

    finish module 4.3

--- Attempt01/ChatClient/src/com/muc/ChatClient.java
+++ Attempt01/ChatClient/src/com/muc/ChatClient.java
@@ -1,7 +1,11 @@
 package com.muc;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.*;
 import java.net.Socket;
+import java.sql.Array;
+import java.util.ArrayList;
 
 public class ChatClient {
     private final int serverPort    ;
@@ -11,6 +15,8 @@ public class ChatClient {
     private InputStream serverIn;
     private BufferedReader bufferedIn;
 
+    private ArrayList<UserStatusListener> userStatusListeners = new ArrayList<>();
+
     public ChatClient(String serverName, int serverPort) {
         this.serverName = serverName;
         this.serverPort = serverPort;
@@ -18,6 +24,17 @@ public class ChatClient {
 
     public static void main(String[] args) throws IOException {
         ChatClient client = new ChatClient("localhost", 8818);
+        client.addUserStatusListener(new UserStatusListener() {
+            @Override
+            public void online(String login) {
+                System.out.println("ONLINE: " + login);
+            }
+
+            @Override
+            public void offline(String login) {
+                System.out.println("OFFLINE: " + login);
+            }
+        });
         if(!client.connect()){
             System.err.println("Connect failed!");
         }else{
@@ -27,18 +44,73 @@ public class ChatClient {
             }else{
                 System.err.println("Login failed.");
             }
+//            client.logoff();
         }
+    }
+
+    private void logoff() throws IOException {
+        String cmd = "logoff\n";
+        serverOut.write(cmd.getBytes());
     }
 
     private boolean login(String login, String password) throws IOException {
         String cmd = "login " + login + " " + password  +"\n";
         serverOut.write(cmd.getBytes());
         String response = bufferedIn.readLine();
-
+        System.out.println("Response: " + response);
         if("ok login".equalsIgnoreCase(response)){
+            startMessageReader();
             return true;
         }else{
             return false;
+        }
+    }
+
+    private void startMessageReader() {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                readMessageLoop();
+            }
+        };
+        t.start();
+    }
+
+    private void readMessageLoop() {
+        try{
+            String line;
+            while( (line = bufferedIn.readLine()) != null) {
+                String[] tokens = StringUtils.split(line);
+                if (tokens != null && tokens.length > 0) {
+                    String cmd = tokens[0];
+                    if ("online".equalsIgnoreCase(cmd)){
+                        handleOnline(tokens);
+                    }else if("offline".equalsIgnoreCase(cmd)){
+                        handleOffline(tokens);
+                    }
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            try {
+                socket.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    private void handleOffline(String[] tokens) {
+        String login = tokens[1];
+        for(UserStatusListener listener : userStatusListeners){
+            listener.offline(login);
+        }
+    }
+
+    private void handleOnline(String[] tokens) {
+        String login = tokens[1];
+        for(UserStatusListener listener : userStatusListeners){
+            listener.online(login);
         }
     }
 
@@ -54,6 +126,12 @@ public class ChatClient {
             e.printStackTrace();
         }
         return false;
+    }
 
+    public void addUserStatusListener(UserStatusListener listener){
+        userStatusListeners.add(listener);
+    }
+    public void removeUserStatusListener(UserStatusListener listener){
+        userStatusListeners.remove(listener);
     }
 }
